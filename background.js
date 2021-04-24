90c57b8272d32712511c14290c001ab75d32b3b9
Seulgi_LEE <dl4459@naver.com>
2021-04-24 21:59:02 +0900

    initial setting

--- background.js
+++ background.js
@@ -1,6 +1,1 @@
-let color = '#3aa757';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
+// 백그라운드에서 참조할 파일
