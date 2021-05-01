42db066f5d65eacc5bf4e6591dd8f5645a59ef71
zzz664 <zzz664>
2021-05-02 00:21:08 +0900

    MongoDB연동, Cluster연동

--- server/server.js
+++ server/server.js
@@ -7,6 +7,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true}));
 // 이후 데이터를 주고 받을 때 post방식을 사용하기 위한 초기 설정입니다. 무시하시면 됩니다.
 
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://admin:osspcodenation@ossp-codenation.6ezqi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB connected!'))
+.catch(() => console.log(err)); 
+
 app.get('/api/file', (req, res)=>{
     res.send({ message: '자료조사 봇 서버에 오신걸 환영합니다.'});
 })
