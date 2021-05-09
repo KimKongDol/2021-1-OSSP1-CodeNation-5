41e503e89e852f7268983b95d5cbe620d650f381
Seulgi_LEE <dl4459@naver.com>
2021-05-10 03:08:38 +0900

    popup창 수정

--- client/script.js
+++ client/script.js
@@ -6,3 +6,25 @@ const search = document.querySelector("#search"),
 search.click(function() {
     searchButton.submit();
 })
+
+const triggers = Array.from(document.querySelectorAll('[data-toggle="collapse"]'));
+
+window.addEventListener('click', (ev) => {
+  const elm = ev.target;
+  if (triggers.includes(elm)) {
+    const selector = elm.getAttribute('data-target');
+    collapse(selector, 'toggle');
+  }
+}, false);
+
+const fnmap = {
+  'toggle': 'toggle',
+  'show': 'add',
+  'hide': 'remove'
+};
+const collapse = (selector, cmd) => {
+  const targets = Array.from(document.querySelectorAll(selector));
+  targets.forEach(target => {
+    target.classList[fnmap[cmd]]('show');
+  });
+}
