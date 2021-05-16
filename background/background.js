b12c751fe96c52710295cca8216d83b3e4c3026b
zzz664 <zzz664>
2021-05-17 00:02:18 +0900

    Client id 수정 (OAuth2.0관련)

--- background/background.js
+++ background/background.js
@@ -1,7 +1,3 @@
-chrome.identity.getAuthToken({interactive: true}, function(token) {
-    console.log('got the token' + token);
-})
-
 const API_KEY = 'AIzaSyCYzO2cRtI0XnpMAU6kvSGAYp-wGFLEThU';
 const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
 
@@ -16,8 +12,8 @@ function onGapiLoad()
             gapi.auth.setToken({
                 'access_token': token
             });
-        })
-        
+        });
+
     }, function(error) {
         console.log('gapi init fail', error);
     })
