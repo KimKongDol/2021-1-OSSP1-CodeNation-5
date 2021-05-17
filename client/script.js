1cc603a534895bfe63c634fc4b9f9a46c9369e3f
Seulgi_LEE <dl4459@naver.com>
2021-05-17 12:51:47 +0900

    전체 merge 사항

--- client/script.js
+++ client/script.js
@@ -7,6 +7,32 @@ search.click(function () {
     searchButton.submit();
 });
 
+const triggers = Array.from(document.querySelectorAll('[data-toggle="collapse"]'));
+
+window.addEventListener(
+    'click',
+    (ev) => {
+        const elm = ev.target;
+        if (triggers.includes(elm)) {
+            const selector = elm.getAttribute('data-target');
+            collapse(selector, 'toggle');
+        }
+    },
+    false
+);
+
+const fnmap = {
+    toggle: 'toggle',
+    show: 'add',
+    hide: 'remove',
+};
+const collapse = (selector, cmd) => {
+    const targets = Array.from(document.querySelectorAll(selector));
+    targets.forEach((target) => {
+        target.classList[fnmap[cmd]]('show');
+    });
+};
+
 document.querySelector('#sign_in').addEventListener('click', function () {
     chrome.runtime.sendMessage({ message: 'get_access_token' }, function (response) {
         if (response.signed_in) {
